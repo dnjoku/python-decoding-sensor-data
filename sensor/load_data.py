8423eaa9a8c6051cc705ac9e1610f9f01d4e66e7
Dan Njoku <dnjoku@ups.com>
2021-12-01 21:19:16 -0500

    fixed typo in step 5

--- sensor/load_data.py
+++ sensor/load_data.py
@@ -7,9 +7,12 @@ def load_sensor_data():
     ssensor_files = glob.glob(os.path.join(os.getcwd(), 'datasets', '*.csv'))
 
     for sensor_file in sensor_files:
-        with open(sensor_file) as data_file:
+        with open(sensor_file ) as data_file:
+            # Create a csv.DictReader
             data_reader = csv.DictReader(data_file, delimiter=',')
+            # Loop over each row dictionary
             for row in data_reader:
+                # Create a list of dictionaries
                 sensor_data.append(row)
-                
-    return sensor_data
+
+    return sensor_data 
