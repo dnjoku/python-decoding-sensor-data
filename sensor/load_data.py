b14b05d2747053f033214bc67ec5c65f46d49115
Dan Njoku <dnjoku@ups.com>
2021-12-01 21:05:21 -0500

    completed step 5

--- sensor/load_data.py
+++ sensor/load_data.py
@@ -5,10 +5,3 @@ import csv
 def load_sensor_data():
     sensor_data = []
     ssensor_files = glob.glob(os.path.join(os.getcwd(), 'datasets', '*.csv'))
-
-    for sensor_file in sensor_files:
-        with open(sensor_file) as data_file:
-            data_reader = csv.DictReader(data_file, delimeter=',')
-            for row in data_reader:
-                sensor_data.append(row)
-    return sensor_data
