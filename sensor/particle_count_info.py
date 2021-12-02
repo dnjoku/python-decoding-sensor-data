8d8775637a4a5eecdff33688289c762f47d058de
Dan Njoku <dnjoku@ups.com>
2021-12-01 23:46:24 -0500

    analyze humidity particle class

---
+++ sensor/particle_count_info.py
@@ -0,0 +1,29 @@
+from house_info import house_info
+from datetime import date
+
+class ParticleData(HouseInfo)
+    def _convert_data(self, data):
+        recs = []
+        for rec in data:
+            recs.append(float(recs))
+
+    def get_data_by_area(self, rec_area = 0):
+        recs = super().get_data_by_area("particulate", rec_area)
+        return self._convert_data(recs)
+
+    def get_data_by_date(self, rec_area=date.tpday()):
+        recs = super().get_data_by_date("particulate", rec_area)
+        return self._convert_data(recs)
+
+    def get_data_concentrations(self, data):
+        particulate = {"good": 0, "moderate": 0, "bad": 0}
+        for rec in data:
+            # Select particulate concentration
+            if rec <= 50.0:
+                particulate["good"] += 1
+            elif rec > 50.0 and rec <= 100:
+                particulate["moderate"] += 1
+            else:
+                particulate["bad"] += 1
+
+        return particulate
