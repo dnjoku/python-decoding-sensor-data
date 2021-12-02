986bb71bc29bdc8126863e4ec657252f7587e280
Dan Njoku <dnjoku@ups.com>
2021-12-01 21:12:51 -0500

    fixed typo in step 5

--- sensor/load_data.py
+++ sensor/load_data.py
@@ -5,6 +5,3 @@ import csv
 def load_sensor_data():
     sensor_data = []
     ssensor_files = glob.glob(os.path.join(os.getcwd(), 'datasets', '*.csv'))
-
-    for sensor_file in sensor_files:
-        with open(sensor_file) as data_file:
